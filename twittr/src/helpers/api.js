c49b27c42f912cbd82354c5de61bf2468b11fd6e
Smyja <akposlive59@gmail.com>
2022-06-23 19:15:36 +0100

    updated url

--- twittr/src/helpers/api.js
+++ twittr/src/helpers/api.js
@@ -1,7 +1,7 @@
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-    : "https://twittrng.herokuapp.com";
+    : "https://jobtweets.xyz";
 
 export const api = {
   jobs:`${baseURL}/twitter/api/v1/get_jobs`
